81ee3bb41960b01df222570cbf5dcc129715a1b4
GieVai\GieVai <gievai@gmail.com>
2017-10-11 20:10:02 +0300

    Web

---
+++ assets/scripts/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".nav-icon").click(function(){
+        $(".nav-list").toggleClass("open");
+        $(".nav-burger-line").toggleClass("open");
+    });
+    
+    $('.bxslider').bxSlider({
+      auto: true,
+      autoControls: true
+    });
+    
+    
+});
+
+
+
+
